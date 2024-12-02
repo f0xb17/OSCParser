f66cec0e37862844d0efd14e21b33ce64a71e77e
fox <102655383+f0xb17@users.noreply.github.com>
2024-12-02 14:18:42 +0100

    Update oscparser.go
    Fixed a typo in the comment. 
    
    Signed-off-by: fox <102655383+f0xb17@users.noreply.github.com>

--- oscparser.go
+++ oscparser.go
@@ -6,7 +6,7 @@ import (
 	"regexp"
 )
 
-// he read_file function reads an .osc file and extracts all the variable names present in the file.
+// The Read_File function reads an .osc file and extracts all the variable names present in the file.
 // It returns a map where the keys are the variable names and the values are boolean true.
 func Read_File(filePath string, pattern string) (map[string]bool, error) {
 	// Open the file and return an error when the file cannot be opened!
